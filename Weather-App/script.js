21de0b671cf5f0260845bcdb4bd00e1062a8ef9f
Himanshu Mishra <himanshu.2010m@gmail.com>
2025-08-06 20:46:06 +0530

    Basic Weather App using API

---
+++ Weather-App/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cityInput = document.getElementById("city-input");
+    const getWeatherButton = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityNameDisplay = document.getElementById("city-name");
+    const temperatureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorMessage = document.getElementById("error-message");
+
+    getWeatherButton.addEventListener("click", () => {
+        cityInput.value.trim()
+        if (!city) {
+            return;            
+        }
+    })
+})
